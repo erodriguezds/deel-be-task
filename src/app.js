16e36cdc460d63d4a0f057a43d4b7f043113e209
Eduardo <eduardo@dynamonet.com.ar>
2022-06-12 14:43:05 -0300

    cors for the react frontend (which is served on a different port)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const { Op } = require("sequelize");
 
 const app = express();
+
+app.use(cors());
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
